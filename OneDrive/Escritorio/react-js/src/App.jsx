0b5dcaf7520500b7ad10fbfc3d01f848916ef42f
Myriam Bibas <pattybibas@gmail.com>
2023-09-26 23:59:44 -0300

    ajuste de nav bar

--- OneDrive/Escritorio/react-js/src/App.jsx
+++ OneDrive/Escritorio/react-js/src/App.jsx
@@ -1,6 +1,6 @@
 import Contacto from "./components/Contacto";
 import ItemDetailContainer from "./components/ItemDetailContainer";
-import ItemListContainer from "./components/ItemListContainer";
+import ItemListContainer from "./components/ItemListContainer ";
 import Navbar from "./components/Navbar";
 import "./main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
