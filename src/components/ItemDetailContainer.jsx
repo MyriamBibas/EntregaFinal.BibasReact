3a9945203497d91b47be8ff7b3b4e567940f2339
Myriam Bibas <pattybibas@gmail.com>
2023-10-14 12:18:15 -0300

    firebase

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
-import data from "../data/data.json";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
+
 
 
 const ItemDetailContainer = () => {
+
   const [item, setItem] = useState(null);
-  const { id } = useParams();
+  const { id } = useParams().id;
 
   useEffect(() => {
-    const itemData = data.find((item) => item.id === id);
-    setItem(itemData);
-  }, [id]);
 
-  return (
-    <div>
+    const docRef = doc(db, "productos", id);
+    getDoc(docRef)
+      .then((resp) => {
+        setItem(
+          { ...resp.data(), id: resp.id }
+        );
+      })
+
+  }, [id])
+  
+
+return (
+  <div>
       {item && <ItemDetail item={item} />}
-    </div>
-  );
-};
+  </div>
+)
+}
 
-export default ItemDetailContainer;
+export default ItemDetailContainer
--- src/components/ItemListContainer .jsx
+++ src/components/ItemListContainer .jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 const ItemListContainer = () => {
@@ -10,24 +10,25 @@ const ItemListContainer = () => {
   const categoria = useParams().categoria;
 
   useEffect(() => {
-    
-    const productosRef = collection(db, "productos");
-    getDocs(productosRef)
-    .then ((resp) => {
 
-      setProductos(
-        resp.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id }
-            })
-          )
-        })
-   
-     
+    const productosRef = collection(db, "productos");
+    const q = categoria ? query(productosRef, where("categoria", "==", categoria)) : productosRef;
+    getDocs(productosRef)
+      .then((resp) => {
+
+        setProductos(
+          resp.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id }
+          })
+        )
+      })
+
+
   }, [categoria])
 
   return (
     <div>
-      <ItemList productos={productos} />
+      <ItemList productos={productos} titulo={titulo} />
     </div>
   )
 }
